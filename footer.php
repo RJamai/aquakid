708b8fafee665fa674d98de5f707abe25b845341
RJamai <contact@developpeur-frontend.com>
2013-11-06 17:24:59 +0100

    Import des données de base

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="l-footer">
+    <nav class="l-nav l-nav-small clearfix">
+        <a href="faq.html" class="nav-link nav-link-faq">
+            <i class="icon-elephant"></i>
+            FAQ
+        </a>
+        <a href="contact.html" class="nav-link nav-link-contact">
+            <i class="icon-crocodile"></i>
+            Contact
+        </a>
+    </nav>
+    <p class="footer-bottom">AQUAKID 2013 - <a href="mentions-legales.html">Mentions légales</a></p>
+</footer>
---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php the_title(); ?>s</title>
+    <meta name="description" content="La joyeuse bande d'AQUAKID ne va pas tarder à débarquer dans ton quartier. Alors ouvre l'oeil et prépare toi pour la fiesta !">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no">
+
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="apple-touch-icon-precomposed.png" />
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.6.2.custom.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]>
+<p class="browsehappy">Vous utilisez un navigateur <strong>trop vieux</strong>. Nous vous recommandons de le <a href="http://browsehappy.com/">mettre à jour</a> pour pouvoir visualiser le site correctement.</p>
+<![endif]-->
+
+<header class="l-header">
+    <div class="header-backgroundcolor">
+        <a class="header-logo" href="index.html">Aquakid - L'eau officielle des enfants</a>
+    </div>
+    <nav class="l-nav l-constrained clearfix">
+        <a href="apropos.html" class="nav-link nav-link-about">
+            <i class="icon-cat"></i>
+            À propos<br /> de nous
+        </a>
+        <a href="bouteilles-personnages.html" class="nav-link nav-link-bottles">
+            <i class="icon-monkey"></i>
+            Bouteilles et<br /> personnages
+        </a>
+        <a href="eau-magique.html" class="nav-link nav-link-water">
+            <i class="icon-lion"></i>
+            Notre eau<br /> magique
+        </a>
+        <a href="secret-bonheur.html" class="nav-link nav-link-happiness">
+            <i class="icon-duck"></i>
+            Le secret<br /> du bonheur
+        </a>
+        <a href="ou-nous-trouver.html" class="nav-link nav-link-findus is-active">
+            <i class="icon-chick"></i>
+            Où nous<br /> trouver
+        </a>
+    </nav>
+</header>
