788f355bd7938a17ea9176e5ac18d0f83302067f
RJamai <contact@developpeur-frontend.com>
2013-11-12 18:34:28 +0100

    Mise en place des templates
    Mise en place des templates ou nous trouver et début de secret bonheur

--- footer.php
+++ footer.php
@@ -1,13 +1,32 @@
-<footer class="l-footer">
-    <nav class="l-nav l-nav-small clearfix">
-        <a href="faq.html" class="nav-link nav-link-faq">
-            <i class="icon-elephant"></i>
-            FAQ
-        </a>
-        <a href="contact.html" class="nav-link nav-link-contact">
-            <i class="icon-crocodile"></i>
-            Contact
-        </a>
-    </nav>
-    <p class="footer-bottom">AQUAKID 2013 - <a href="mentions-legales.html">Mentions légales</a></p>
-</footer>
+			<footer class="l-footer">
+	       		<nav class="l-nav l-nav-small clearfix">
+	        		<a href="faq.html" class="nav-link nav-link-faq">
+	        			<i class="icon-elephant"></i>
+	        			FAQ
+	        		</a>
+	        		<a href="contact.html" class="nav-link nav-link-contact">
+	        			<i class="icon-crocodile"></i>
+	        			Contact
+	        		</a>
+	        	</nav>
+	        	<p class="footer-bottom">AQUAKID 2013 - <a href="mentions-legales.html">Mentions légales</a></p>
+	        </footer>
+	    </section>
+	    </div><!-- .l-constrained -->
+	
+	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+	    <script src="js/main.js"></script>
+	
+	    <script>
+	        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	        
+	        ga('create', 'UA-37903440-3', 'aquakid.com');
+	        ga('send', 'pageview');
+	        
+	    </script>
+	</body>
+</html>
