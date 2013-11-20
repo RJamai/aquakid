2d3c8631f3508631cdf0938160c3a418cedc825d
RJamai <contact@developpeur-frontend.com>
2013-11-20 12:36:31 +0100

    Correction mineur Custom Type

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@ add_theme_support( 'post-thumbnails' );
 add_action('init', 'my_custom_init');
 function my_custom_init()
 {
-	register_post_type('personnage', array(
-		'label' => __('personnages'),
-		'singular_label' => __('personnage'),
+	register_post_type('Personnage', array(
+		'label' => __('Personnages'),
+		'singular_label' => __('Personnage'),
 		'public' => true,
 		'show_ui' => true,
 		'capability_type' => 'post',
