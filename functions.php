b0f5e42e4bfd9a9430fb8e973a397d8b941f0d67
RJamai <contact@developpeur-frontend.com>
2013-11-20 12:27:32 +0100

    Rajout d'un custom type " personnage "

--- functions.php
+++ functions.php
@@ -1,2 +1,16 @@
 <?php
 add_theme_support( 'post-thumbnails' );
+
+add_action('init', 'my_custom_init');
+function my_custom_init()
+{
+	register_post_type('personnage', array(
+		'label' => __('personnages'),
+		'singular_label' => __('personnage'),
+		'public' => true,
+		'show_ui' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'supports' => array('title', 'excerpt', 'thumbnail')
+	));
+}
--- index.php
+++ index.php
@@ -4,10 +4,10 @@
             <p class="browsehappy">Vous utilisez un navigateur <strong>trop vieux</strong>. Nous vous recommandons de le <a href="http://browsehappy.com/">mettre à jour</a> pour pouvoir visualiser le site correctement.</p>
         <![endif]-->
 		
-  		<a href="apropos.html" class="home-characters">
+  		<a href="page-apropos.php" class="home-characters">
   			<img id="home-characters" src="img/aquakid-eau-officielle-des-enfants.png" alt="Aquakid - L'eau officielle des enfants" />
   		</a>
-  		<a href="apropos.html" class="btn">Entre sur le site !</a>
+  		<a href="page-apropos.php" class="btn">Entre sur le site !</a>
 		
 		<div class="loader" id="loader">
 	    	<i class="loader-item"></i>
